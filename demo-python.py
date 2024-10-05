c4a1f53e40ebfb639d12cf325278fdeaf08b96a6
kaho85 <101995644+kaho85@users.noreply.github.com>
2024-10-05 08:03:05 -0700

    Update demo-python.py

--- demo-python.py
+++ demo-python.py
@@ -1,4 +1,5 @@
 import streamlit as st
 import pandas as pd
 st.write("my first application is done")
+st.write("my very first work on streamlit")
 print("this is my first application and i'm happy")
